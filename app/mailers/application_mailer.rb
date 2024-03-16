4619755dda9b7c62dbbfcc266eb301c5c05fb9c4
thenicholashernandez <thenicholashernandez@gmail.com>
2024-03-16 16:00:13 +0800

    Edit mail address

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,9 +1,9 @@
 class ApplicationMailer < ActionMailer::Base
   # This is for test/development
-  default from: "user@realdomain.com"
+  # default from: "user@realdomain.com"
 
   # This is for production
-  # default from: "thenicholashernandez@gmail.com"
+  default from: "thenicholashernandez@gmail.com"
 
   layout "mailer"
 end
